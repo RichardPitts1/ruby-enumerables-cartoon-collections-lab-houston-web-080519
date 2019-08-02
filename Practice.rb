9bcc67ab3b0871922d59d94e749a1b0573e0b385
Richard Pitts <richardepitts@gmail.com>
2019-08-02 20:03:17 +0000

    Automatically backed up by Learn

--- Practice.rb
+++ Practice.rb
@@ -3,5 +3,5 @@ dwarvesArray = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarvesArray)
   i = 0 
   while i < dwarvesArray.length 
-   puts "#{i + 1}. #{dwarvesArray}[i]"
+   puts "#{i + 1}. #{dwarvesArray[i]}"
 end 
